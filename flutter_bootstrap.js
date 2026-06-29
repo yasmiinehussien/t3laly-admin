5ef7755a71a8c7c222346ae7a3719ab43dac0dfe
Yasmine Hussien <yasmiinehussien@gmail.com>
2026-06-29 04:04:00 +0300

    deploy github pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2392378879" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1606953290" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
